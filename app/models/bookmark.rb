b999bab143994b6a88604aeaac9e9d0104701e5d
leopaul fabbro <leopaulfabbro@gmail.com>
2021-09-25 12:47:58 +0200

    ending models def

---
+++ app/models/bookmark.rb
@@ -0,0 +1,12 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :movie, presence: true
+
+  validates :list, presence: true
+
+  validates :comment, length: { minimum: 6 }
+
+  validates :movie, uniqueness: { scope: :list }
+end
